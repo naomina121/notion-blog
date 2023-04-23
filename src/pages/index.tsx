d2b349c5a2a42967111e92f98fb85901142085c9
naomi <naomina@me.com>
2023-04-24 08:59:40 +0900

    ブログトップページをレイアウトする

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,124 +1,67 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import React from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+const index = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
+    <div className='flex h-full min-h-screen w-full flex-col bg-gray-800'>
+      <div className='flex-glow mx-auto h-full w-full max-w-6xl flex-1 bg-white p-16'>
+        <header>
+          <h1 className='font-sans text-4xl font-bold'>Notion BLOG</h1>
+        </header>
+        <main className='p-16'>
+          <ul>
+            <li className='mb-8 border-b-[1px] border-dotted border-gray-300 pb-4'>
+              <div className='mb-2 text-sm text-gray-500'>
+                <time className='mr-4' dateTime='2022-07-07'>
+                  2022/7/7
+                </time>
+                <span>プログラミング</span>
+              </div>
+              <h2 className='mb-4 font-sans text-2xl font-semibold'>
+                <a href='#' className='hover:opacity-80'>初学者が学ぶべき、オブジェクト指向解説</a>
+              </h2>
+              <p className='mb-4'>
+                初学者こそ、オブジェクト指向について、学んだほうがいいと感じてきたのでそれについて語っていこうと思う。
+              </p>
+              <a href='#' className='bg-lime-600 text-sm text-white text-center p-1 w-full max-w-[120px] block hover:opacity-80'>続きを読む</a>
+            </li>
+            <li className='mb-8 border-b-[1px] border-dotted border-gray-300 pb-4'>
+              <div className='mb-2 text-sm text-gray-500'>
+                <time className='mr-4' dateTime='2018-07-07'>
+                  2018/7/7
+                </time>
+                <span>機械学習</span>
+              </div>
+              <h2 className='mb-4 font-sans text-2xl font-semibold'>
+                <a href='#' className='hover:opacity-80'>ChatGPTを0から作ったことを記事にまとめてみた</a>
+              </h2>
+              <p className='mb-4'>
+                最近、流行りのChatGPTを自分でも作れないかを検討してみたら意外と簡単にできたので驚いた。今回はそれを記事にしていこうと思う。
+              </p>
+              <a href='#' className='bg-lime-600 text-sm text-white text-center p-1 w-full max-w-[120px] block hover:opacity-80'>続きを読む</a>
+            </li>
+            <li className='mb-8 border-b-[1px] border-dotted border-gray-300 pb-4'>
+              <div className='mb-2 text-sm text-gray-500'>
+                <time className='mr-4' dateTime='2023-07-07'>
+                  2023/7/7
+                </time>
+                <span>ネットワーク</span>
+              </div>
+              <h2 className='mb-4 font-sans text-2xl font-semibold'>
+                <a href='#' className='hover:opacity-80'>HTTPSの通信についての流れを図解で解説</a>
+              </h2>
+              <p className='mb-4'>
+                ６歳児の娘が、HTTPS通信について興味を持ち出したので、わかりやすく解説するとしよう。
+              </p>
+              <a href='#' className='bg-lime-600 text-sm text-white text-center p-1 w-full max-w-[120px] block hover:opacity-80'>続きを読む</a>
+            </li>
+          </ul>
+        </main>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <footer className='py-2 px-4 mx-auto w-full max-w-6xl bg-white text-center'>
+        <small className='text-sm'>&copy; Notion BLOG.</small>
+      </footer>
+    </div>
   )
 }
+
+export default index
